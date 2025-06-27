34dc66ad8ea8028c979221b6bf7552bc93285982
VenobyTo <elarjunimedamine@gmail.com>
2025-06-27 18:11:23 +0200

    Classes Python

---
+++ simple_math.py
@@ -0,0 +1,7 @@
+class SimpleMath:
+    @staticmethod
+    def addition(a, b):
+        return a + b
+    @staticmethod
+    def soustraction(a, b):
+        return a - b
---
+++ test_simple_math.py
@@ -0,0 +1,12 @@
+import unittest
+from simple_math import SimpleMath
+
+class TestSimpleMath(unittest.TestCase):
+    def test_addition(self):
+        self.assertEqual(SimpleMath.addition(2, 3), 5)
+
+if __name__ == '__main__':
+    unittest.main()
+
+def test_soustraction(self):
+    self.assertEqual(SimpleMath.soustraction(5, 2), 3)
